7cf9442366c868c88d84e1e3f2e383d4121e9704
TomarJatin <jt24072002@gmail.com>
2023-12-05 00:14:18 +0530

    contract functions added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const contractAddress = "0xF2893d1112b11c73e434dD6767240e50bc9465AB";
+  const address = useAddress();
+  const { contract } = useContract(contractAddress);
+  const { data, isLoading, error } = useContractRead(contract, "getTodo", []);
+
   return (
     <main >
-      <h1 className="text-7xl">yo</h1>
-      {/* <ConnectWallet
+      <h1>{address}</h1>
+      <ConnectWallet
               dropdownPosition={{
                 side: "bottom",
                 align: "center",
               }}
-            /> */}
+            />
+            {/* <Web3Button
+              contractAddress={contractAddress}
+              action={(contract) => contract.call("setTodo", input)}
+              accentColor="#1ce"
+            >
+              Set Todo
+            </Web3Button> */}
     </main>
   );
 };
