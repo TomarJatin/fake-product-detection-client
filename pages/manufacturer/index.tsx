860d3b43bb29ef4c07e7b8b5f4e177b1f0255c0f
TomarJatin <jt24072002@gmail.com>
2023-12-05 02:28:09 +0530

    all required files intiated

--- pages/manufacturer/index.tsx
+++ pages/manufacturer/index.tsx
@@ -2,35 +2,34 @@ import { ConnectWallet, useAddress, useContract, useContractRead } from "@thirdw
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { NextPage } from "next";
-import { useRouter } from "next/router";
 
-const Home: NextPage = () => {
+const Manufacturer: NextPage = () => {
   const contractAddress = "0x2c9F16B88F3AA7b4eCA45115eDedE00172c9E44f";
   const address = useAddress();
-  const router = useRouter();
   const { contract } = useContract(contractAddress);
   // const { data, isLoading, error } = useContractRead(contract, "getTodo", []);
 
   return (
     <main >
-      
+      <h1>Manufacturer</h1>
       <ConnectWallet
               dropdownPosition={{
                 side: "bottom",
                 align: "center",
               }}
             />
-            {
-              address ? (
-                <div>
-                  <button onClick={() => router.push("/retailer")}>Retailer</button>
-                  <button onClick={() => router.push("/customer")}>Customer</button>
-                  <button onClick={() => router.push("/manufacturer")}>Manufacturer</button>
-                </div>
-              ): null
-            }
+
+            
+
+            {/* <Web3Button
+              contractAddress={contractAddress}
+              action={(contract) => contract.call("setTodo", input)}
+              accentColor="#1ce"
+            >
+              Set Todo
+            </Web3Button> */}
     </main>
   );
 };
 
-export default Home;
+export default Manufacturer;
